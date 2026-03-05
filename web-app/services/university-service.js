7c596749ae413dafb537702bcea0d8c1779b5cb7
Aditya Singh <2301201182@krmu.edu.in>
2026-03-05 12:52:48 +0530

    Improve mobile navigation menu interactions

--- web-app/services/university-service.js
+++ web-app/services/university-service.js
@@ -159,3 +159,5 @@ module.exports = { issueCertificate, getCertificateDataforDashboard, revokeCerti
 /* minor update: 2026-03-04 17:15:01 */
 
 /* minor update: 2026-03-04 14:45:28 */
+
+/* minor update: 2026-03-05 12:52:48 */
