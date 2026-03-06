10b03ea2335409eef9587f070031640b31357a6b
Aditya Singh <2301201182@krmu.edu.in>
2026-03-06 09:39:19 +0530

    Add detailed documentation to chaincode functions

--- web-app/services/university-service.js
+++ web-app/services/university-service.js
@@ -171,3 +171,5 @@ module.exports = { issueCertificate, getCertificateDataforDashboard, revokeCerti
 /* minor update: 2026-03-06 11:54:13 */
 
 /* minor update: 2026-03-06 13:02:14 */
+
+/* minor update: 2026-03-06 09:39:19 */
