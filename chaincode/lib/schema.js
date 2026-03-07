39d3515d50f8a8803875e7f0f75ff7706d5c017c
Aditya Singh <2301201182@krmu.edu.in>
2026-03-07 22:25:42 +0530

    feat: add logic implementation for schema.js

--- chaincode/lib/schema.js
+++ chaincode/lib/schema.js
@@ -1,18 +1,13 @@
-// eg UniversitySchema = {
-//     dataType : "schema",
-//     id: "v1",
-//     ordering: ["studentName", "studentEmail", "universityName", "universityEmail", "major", "departmentName", "cgpa"],
-//     certificateType: "university degree"
-// }
+
+
+
+
+
+
 
 
 class Schema {
-    /**
-    * Schema for a certain type of certificate
-    * @param {String} certificateType 
-    * @param {String} id 
-    * @param {String[]} ordering 
-    */
+    
     constructor(certificateType, id, ordering){
         this.certificateType = certificateType;
         this.id = id;
@@ -22,11 +17,7 @@ class Schema {
     
     
     
-    /**
-    * Instantiate object from json argument. 
-    * @param {json} data json data of a Profile instance 
-    * @returns {UniversityProfile} instantiated University Profile object. 
-    */
+    
     static deserialize(data) {
         return new Schema(data.certificateType, data.id, data.ordering);
     }
