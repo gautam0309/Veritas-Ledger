719dc4996170ca084c2181fe45d87763aea6be0f
Aditya Singh <2301201182@krmu.edu.in>
2026-03-09 20:36:00 +0530

    feat: add logic implementation for express-session-loader.js

--- web-app/loaders/express-session-loader.js
+++ web-app/loaders/express-session-loader.js
@@ -7,16 +7,16 @@ const mongoose = require("../database/mongoose");
 const mongoStore = connectMongo(expressSession);
 
 let expessSessionConfig = {
-    name: 'session_id', //This will need to be sent with all ajax cals to verify session/authenticate user.
+    name: 'session_id', 
     secret: config.expressSessionSecret,
     resave: false,
     httpOnly: true,
     saveUninitialized: false,
     cookie: {
         maxAge: 86400000,
-        httpOnly: true, // Prevents client-side scripts from reading the cookie
-        sameSite: 'lax', // Helps mitigate basic CSRF attacks
-        secure: process.env.NODE_ENV === 'production' // Only send cookie over HTTPS in production
+        httpOnly: true, 
+        sameSite: 'lax', 
+        secure: process.env.NODE_ENV === 'production' 
     },
     store: new mongoStore({
         mongooseConnection: mongoose.connection,
@@ -28,4 +28,3 @@ let sessionMiddleware = expressSession(expessSessionConfig);
 
 
 module.exports = sessionMiddleware;
-//must come after mongoDB is loaded.
