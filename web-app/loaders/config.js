19460f37211393cf70efac8d91816c6e5a5fe981
Aditya Singh <2301201182@krmu.edu.in>
2026-03-09 19:00:00 +0530

    feat: add logic implementation for config.js

--- web-app/loaders/config.js
+++ web-app/loaders/config.js
@@ -1,12 +1,12 @@
-//add configuration files
-//initialize environment variables
-//try to use the export from this file instead of touching process.env directly.
+
+
+
 
 
 const env = process.env.NODE_ENV || 'development';
 
 if (env === 'development') {
-    process.env.MONGODB_URI = process.env.MONGODB_URI_LOCAL;  //in case of dev, connect to local URI.
+    process.env.MONGODB_URI = process.env.MONGODB_URI_LOCAL;  
     process.env.NODE_ENV = 'development';
 }
 
