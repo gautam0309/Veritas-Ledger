2a4b0f2a757126ae2b07dcdfd1e3d065a86606ee
Aditya Singh <2301201182@krmu.edu.in>
2026-03-10 22:41:30 +0530

    feat: add logic implementation for certificate.js

--- chaincode/lib/certificate.js
+++ chaincode/lib/certificate.js
@@ -2,21 +2,10 @@
 
 
 class Certificate {
-    /**
-     * constructor for the certificate transaction object. This will be written to the blockchain ledger for each
-     * certificate issued. 
-     *  
-     * @param {String} certHash - Hash created from the certificate data. 
-     * @param {String} universitySignature - Signature of @certHash signed by private key of issuer(university)
-     * @param {String} studentSignature - Signature of @certHash signed by private key of holder(student)
-     * @param {String} dateOfIssuing - Date the certificate was issued
-     * @param {String} certUUID - UUID for a certificate (automatically generated. Must match with database entry)
-     * @param {String} universityPK - Public key or public ID of issuer account
-     * @param {String} studentPK - Public key or public ID of student account 
-     */
+    
 
-    //todo: universityPK and studentPK should ideally be public keys. If you can't accomplish this, look into using 
-    // some kind of UUID instead. 
+    
+    
     constructor(certHash, universitySignature, studentSignature, dateOfIssuing, certUUID, universityPK, studentPK, issuerEmail = "") {
         this.certHash = certHash;
         this.universityPK = universityPK;
@@ -34,11 +23,7 @@ class Certificate {
 
 
 
-    /**
-     * Instantiate object from json argument. 
-     * @param {json} data json data of a Product instance 
-     * @returns {Certificate} instantiated Certificate object. 
-     */
+    
 
     static deserialize(data) {
         const cert = new Certificate(data.certHash, data.universitySignature, data.studentSignature, data.dateOfIssuing, data.certUUID, data.universityPK, data.studentPK, data.issuerEmail);
@@ -49,7 +34,7 @@ class Certificate {
     }
 
 
-    //todo: Add validation of some kind to see that the signatures match and stuff. (LATER)
+    
 }
 
 module.exports = Certificate;
