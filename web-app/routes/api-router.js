7b4a078998fa04195f588faca22a9d661dae85da
Aditya Singh <2301201182@krmu.edu.in>
2026-03-12 01:24:00 +0530

    feat: add logic implementation for api-router.js

--- web-app/routes/api-router.js
+++ web-app/routes/api-router.js
@@ -3,12 +3,12 @@ var router = express.Router();
 const apiController = require("../controllers/api-controller");
 const studentMiddleware = require('../middleware/student-middleware');
 
-/* GET users listing. */
+
 router.get('/generateProof', studentMiddleware.authenticateLogin, apiController.getGenerateProof);
 router.post('/verify', apiController.postVerifyCert);
 
 
 
-// error handler
+
 router.use(apiController.apiErrorHandler);
 module.exports = router;
