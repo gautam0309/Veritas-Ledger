459a5e78959d85ab4fdfdcaf829a73fe07ea57d1
Aditya Singh <2301201182@krmu.edu.in>
2026-03-12 23:48:00 +0530

    docs: add logic explanations to partner.js

--- web-app/public/js/partner.js
+++ web-app/public/js/partner.js
@@ -1,19 +1,19 @@
 'use strict';
 
-let apiUrl = location.protocol + '
+let apiUrl = location.protocol + '//' + location.host + '/api/';
 
-
+//check user input and call server
 $('.sign-in-partner').click(function() {
 
-    
+    //get user input data
     let formPartnerId = $('.partner-id input').val();
     let formCardId = $('.card-id input').val();
 
-    
+    //create json data
     let inputData = '{' + '"partnerid" : "' + formPartnerId + '", ' + '"cardid" : "' + formCardId + '"}';
     console.log(inputData);
 
-    
+    //make ajax call
     $.ajax({
         type: 'POST',
         url: apiUrl + 'partnerData',
@@ -21,21 +21,21 @@ $('.sign-in-partner').click(function() {
         dataType: 'json',
         contentType: 'application/json',
         beforeSend: function() {
-            
+            //display loading
             document.getElementById('loader').style.display = 'block';
         },
         success: function(data) {
 
-            
+            //remove loader
             document.getElementById('loader').style.display = 'none';
 
-            
+            //check data for error
             if (data.error) {
                 alert(data.error);
 
             } else {
 
-                
+                //update heading
                 $('.heading').html(function() {
                     let str = '<h2><b> ' + data.name + ' </b></h2>';
                     str = str + '<h2><b> ' + data.id + ' </b></h2>';
@@ -43,7 +43,7 @@ $('.sign-in-partner').click(function() {
                     return str;
                 });
 
-                
+                //update dashboard
                 $('.dashboards').html(function() {
                     let str = '';
                     str = str + '<h5>Total points allocated to customers: ' + data.pointsGiven + ' </h5>';
@@ -51,7 +51,7 @@ $('.sign-in-partner').click(function() {
                     return str;
                 });
 
-                
+                //update earn points transaction
                 $('.points-allocated-transactions').html(function() {
                     let str = '';
                     let transactionData = data.earnPointsResults;
@@ -62,7 +62,7 @@ $('.sign-in-partner').click(function() {
                     return str;
                 });
 
-                
+                //update use points transaction
                 $('.points-redeemed-transactions').html(function() {
                     let str = '';
                     let transactionData = data.usePointsResults;
@@ -73,15 +73,15 @@ $('.sign-in-partner').click(function() {
                     return str;
                 });
 
-                
+                //remove login section
                 document.getElementById('loginSection').style.display = 'none';
-                
+                //display transaction section
                 document.getElementById('transactionSection').style.display = 'block';
             }
 
         },
         error: function(jqXHR, textStatus, errorThrown) {
-            
+            //reload on error
             alert('Error: Try again');
             console.log(errorThrown);
             console.log(textStatus);
