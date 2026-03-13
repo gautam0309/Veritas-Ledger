b604f48b417a330aeeee6ce49c9f9889a40c0ecf
Aditya Singh <2301201182@krmu.edu.in>
2026-03-13 21:40:00 +0530

    feat: add logic implementation for index-router.js

--- web-app/routes/index-router.js
+++ web-app/routes/index-router.js
@@ -4,7 +4,7 @@ const router = express.Router();
 let title = "Blockchain Certificate";
 let root = "index";
 
-/* GET home page. */
+
 router.get('/', function(req, res, next) { res.render('index', {   title, root,
     logInType: req.session.user_type || "none"
 });});
