cec6173c129eb164f541afd5ebd3502b8b70bc20
Aditya Singh <2301201182@krmu.edu.in>
2026-03-26 23:00:00 +0530

    docs: add logic explanations to index.js

--- chaincode/index.js
+++ chaincode/index.js
@@ -2,7 +2,7 @@
 
 const FabCar = require('./lib/educert_contract');
 
-
-
+//NOTE: Estore was changed to Educert.
+//Todo: During chaincode invocation, each chaincode is given a name. Find out where that name originates from. 
 module.exports.FabCar = FabCar;
 module.exports.contracts = [FabCar];
