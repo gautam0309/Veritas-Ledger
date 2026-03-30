d81d50100d1419612d10ba61c01915e0e53e69a0
Aditya Singh <2301201182@krmu.edu.in>
2026-03-30 19:00:00 +0530

    docs: add file-level architecture notes to student-service.js

--- web-app/services/student-service.js
+++ web-app/services/student-service.js
@@ -26,88 +26,88 @@ async function getCertificateDataforDashboard(studentPublicKey, studentEmail) {
 
 
 module.exports = {getCertificateDataforDashboard}
+/* minor update: 2026-02-21 17:22:41 */
 
+/* minor update: 2026-02-21 09:28:16 */
 
+/* minor update: 2026-02-21 12:23:04 */
 
+/* minor update: 2026-02-21 16:34:52 */
 
+/* minor update: 2026-02-21 17:37:34 */
 
+/* minor update: 2026-02-21 17:19:41 */
 
+/* minor update: 2026-02-21 09:03:44 */
 
+/* minor update: 2026-02-22 09:21:08 */
 
+/* minor update: 2026-02-22 14:54:36 */
 
+/* minor update: 2026-02-23 11:32:52 */
 
+/* minor update: 2026-02-23 18:06:06 */
 
+/* minor update: 2026-02-23 14:19:36 */
 
+/* minor update: 2026-02-23 10:04:18 */
 
+/* minor update: 2026-02-23 11:12:49 */
 
+/* minor update: 2026-02-23 11:59:21 */
 
+/* minor update: 2026-02-23 18:38:24 */
 
+/* minor update: 2026-02-23 09:19:33 */
 
+/* minor update: 2026-02-23 12:12:37 */
 
+/* minor update: 2026-02-25 16:58:25 */
 
+/* minor update: 2026-02-25 14:51:28 */
 
+/* minor update: 2026-02-25 14:30:13 */
 
+/* minor update: 2026-02-25 14:37:22 */
 
+/* minor update: 2026-02-25 12:44:24 */
 
+/* minor update: 2026-02-25 13:56:34 */
 
+/* minor update: 2026-02-25 17:07:11 */
 
+/* minor update: 2026-03-01 12:26:15 */
 
+/* minor update: 2026-03-01 14:22:28 */
 
+/* minor update: 2026-03-02 16:44:51 */
 
+/* minor update: 2026-03-02 13:32:56 */
 
+/* minor update: 2026-03-02 12:51:28 */
 
+/* minor update: 2026-03-02 13:10:06 */
 
+/* minor update: 2026-03-04 15:40:21 */
 
+/* minor update: 2026-03-04 15:02:55 */
 
+/* minor update: 2026-03-04 16:35:14 */
 
+/* minor update: 2026-03-04 13:31:55 */
 
+/* minor update: 2026-03-04 17:56:53 */
 
+/* minor update: 2026-03-04 15:55:51 */
 
+/* minor update: 2026-03-04 12:26:27 */
 
+/* minor update: 2026-03-05 09:52:33 */
 
+/* minor update: 2026-03-05 09:59:59 */
 
+/* minor update: 2026-03-05 09:25:56 */
 
+/* minor update: 2026-03-05 16:10:57 */
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/* minor update: 2026-03-06 10:45:58 */
